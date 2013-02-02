cbe08b032705da17f8bcd9cb4c55bd39243eaa1e
Eugene Shih <eugene.shih@qrclab.com>
2013-02-01 21:43:16 -0500

    I think the solution is done, but should probably do some more testing

--- squareroot/src/main.cc
+++ squareroot/src/main.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <cassert>
+// there is a defined somewhere
+#define EPS 0.000001
 
 float squareroot(float in);
 
@@ -13,7 +16,9 @@ main(void)
         i++) {
         float val = sqrt(vals[i]);
         std::cout << "sqrt(" << vals[i] << "): " << val << std::endl;
-//        assert(squareroot == val);
+        std::cout << "squareroot(" << vals[i] << "): "
+                  << squareroot(vals[i]) << std::endl;        
+        assert(std::abs(squareroot(vals[i]) - val) <= EPS);
     }
         
     return 0;
@@ -21,5 +26,21 @@ main(void)
 
 float squareroot(float in)
 {
-    
+    float low = 0, high = 1;
+    while(std::abs(high*high - low*low) > EPS) {
+//        std::cout << "(" << low << "," << high << ")" << std::endl;
+        if (in > low*low) {
+            if (in < (high*high)) {
+                high = (low + high) / 2;
+            } else {
+                low = high;
+                high = high * 2;
+            }
+        } else if (in == low * low) {
+            return low;
+        } else {
+            assert(false);
+        }
+    }
+    return low;
 }
